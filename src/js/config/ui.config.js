0e9f1e93341663a2f8e42fe6b989717c315a1015
M Sergey <crow.866@gmail.com>
2019-11-28 01:13:08 +0300

    add validate for form

---
+++ src/js/config/ui.config.js
@@ -0,0 +1,11 @@
+'use strict';
+const UI = {
+    form: document.forms[ 'mdbform' ],
+    inputEmail: document.querySelector( '#Form-email1' ),
+    inputPass: document.querySelector( '#Form-pass1' )
+
+};
+
+export default UI;
+
+
---
+++ src/js/views/form.ui.component.js
@@ -0,0 +1,7 @@
+'use strict';
+export function showInputError(  ) {
+
+}
+export function showInputSuccess(  ) {
+    
+}
